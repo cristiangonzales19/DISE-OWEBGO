cbdd13235a4fd92ed9c95216f488013a653597a4
cristian <c-l-126@hotmail.com>
2021-10-09 20:38:32 -0500

    respaldo 12

---
+++ eventosteclado/jquery/mijscript.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function(){
+$('input#campo').keyup(function(){
+   alert ('usted dejo de presionar tecla');
+});
+
+});
+    
+/*$(document).ready(function(){
+   $('input#campo').keydown(function(){
+      alert ('usted a presionado una tecla');
+   });
+   
+   });
+      
+   $(document).ready(function(){
+      $('input#campo').keypress(function(){
+         alert ('presionaste una tecla');
+      });
+      
+      });*/
---
+++ sesion7/js/mijavascript.js
@@ -0,0 +1,25 @@
+function compara(){
+    var a=2;
+    var b=2;
+    if(a== b)
+    alert ("a y b son iguales");
+    else
+    alert  ("a y b son diferentes");
+
+}
+function resta(){
+
+    var rest= x - y;
+    document.write ("<h2>la resta es " + rest + "</h2>");
+}
+
+function suma(a,b){
+var suma =a + b;
+document.getElementById("sumar").innerHTML="la suma es " + suma;
+
+}
+
+function escribir() {
+
+    valor = document.getElementById('entrada').value; document.getElementById('contenido').innerHTML=' '+valor; }
+    
